db2d132400c77deaf184a14b4e2072ba27fc6280
Viesturs Gribulis <viesturs.gribulis@gmail.com>
2025-01-25 00:48:38 +0200

    initial commit

---
+++ Assets/Scripts/CannonController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonController : MonoBehaviour
+{
+    public float minAngle = -60f; // Minimum rotation angle
+    public float maxAngle = 60f;  // Maximum rotation angle
+
+    void Update()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = mousePos - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        // Clamp the angle within the allowed range
+        angle = Mathf.Clamp(angle - 90, minAngle, maxAngle);
+
+        // Apply the rotation
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+}
---
+++ Assets/Scripts/ShootBubble.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootBubble : MonoBehaviour
+{
+    public GameObject bubblePrefab; // Assign your bubble prefab here
+    public float shootForce = 10f;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) // Left mouse click
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bubble = Instantiate(bubblePrefab, transform.position, transform.rotation);
+        Rigidbody2D rb = bubble.GetComponent<Rigidbody2D>();
+        rb.AddForce(transform.up * shootForce, ForceMode2D.Impulse);
+    }
+}
